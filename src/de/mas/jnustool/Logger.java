62aa7598cffe40ba0760653a9b754408f17a8118
Timothy Redaelli <timothy.redaelli@gmail.com>
2016-05-09 15:43:29 +0200

    Add support for headless system

--- src/de/mas/jnustool/Logger.java
+++ src/de/mas/jnustool/Logger.java
@@ -9,13 +9,17 @@ import de.mas.jnustool.util.Settings;
 public class Logger {
 
 	public static void log(String string) {
-		NUSGUI.output.append(string + "\n");
-		NUSGUI.output.setCaretPosition(NUSGUI.output.getDocument().getLength());
+		if(!java.awt.GraphicsEnvironment.isHeadless()){
+			NUSGUI.output.append(string + "\n");
+			NUSGUI.output.setCaretPosition(NUSGUI.output.getDocument().getLength());
+		}
 		if(Settings.logToPrintLn) System.out.println(string);		
 	}
 
 	public static void messageBox(String string) {
-		JOptionPane.showMessageDialog(new JFrame(), string);		
+		if(!java.awt.GraphicsEnvironment.isHeadless()){
+			JOptionPane.showMessageDialog(new JFrame(), string);
+		}
 	}
 	
 }
